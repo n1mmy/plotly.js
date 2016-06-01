513aaec636a7972819c0c72f07b0fb1c7eec50c5
Étienne Tétreault-Pinard <etienne@plot.ly>
2016-06-01 17:25:02 -0400

    add jasmine tests for mapbox (first pass)

---
+++ test/jasmine/tests/scattermapbox_test.js
@@ -0,0 +1,156 @@
+var Plotly = require('@lib');
+// var Lib = require('@src/lib');
+var ScatterMapbox = require('@src/traces/scattermapbox');
+
+// var d3 = require('d3');
+var createGraphDiv = require('../assets/create_graph_div');
+var destroyGraphDiv = require('../assets/destroy_graph_div');
+var customMatchers = require('../assets/custom_matchers');
+
+
+describe('scattermapbox defaults', function() {
+    'use strict';
+
+});
+
+describe('scattermapbox calc', function() {
+    'use strict';
+
+//     var calc = ScatterMapbox.calc;
+
+    beforeAll(function() {
+        jasmine.addMatchers(customMatchers);
+    });
+
+});
+
+describe('scattermapbox convert', function() {
+    'use strict';
+
+});
+
+describe('scattermapbox plot', function() {
+    'use strict';
+
+
+});
+
+describe('scattermapbox hover', function() {
+    'use strict';
+
+    var hoverPoints = ScatterMapbox.hoverPoints;
+
+    var gd, pointData;
+
+    beforeAll(function(done) {
+        jasmine.addMatchers(customMatchers);
+
+        gd = createGraphDiv();
+
+        var data = [{
+            type: 'scattermapbox',
+            lon: [10, 20, 30],
+            lat: [10, 20, 30],
+            text: ['A', 'B', 'C']
+        }];
+
+        Plotly.plot(gd, data).then(done);
+    });
+
+    beforeEach(function() {
+        var cd = gd.calcdata,
+            mapbox = gd._fullLayout.mapbox._subplot;
+
+        pointData = {
+            index: false,
+            distance: 20,
+            cd: cd[0],
+            trace: cd[0][0].trace,
+            xa: mapbox.xaxis,
+            ya: mapbox.yaxis
+        };
+    });
+
+    afterAll(function() {
+        Plotly.purge(gd);
+        destroyGraphDiv();
+    });
+
+    it('should generate hover label info (base case)', function() {
+        var xval = 11,
+            yval = 11;
+
+        var out = hoverPoints(pointData, xval, yval)[0];
+
+        expect(out.index).toEqual(0);
+        expect([out.x0, out.x1, out.y0, out.y1]).toBeCloseToArray([
+            444.444, 446.444, 105.410, 107.410
+        ]);
+        expect(out.extraText).toEqual('(10°, 10°)<br>A');
+        expect(out.color).toEqual('#1f77b4');
+    });
+
+    it('should generate hover label info (positive winding case)', function() {
+        var xval = 11 + 720,
+            yval = 11;
+
+        var out = hoverPoints(pointData, xval, yval)[0];
+
+        expect(out.index).toEqual(0);
+        expect([out.x0, out.x1, out.y0, out.y1]).toBeCloseToArray([
+            2492.444, 2494.444, 105.410, 107.410
+        ]);
+        expect(out.extraText).toEqual('(10°, 10°)<br>A');
+        expect(out.color).toEqual('#1f77b4');
+    });
+
+    it('should generate hover label info (negative winding case)', function() {
+        var xval = 11 - 1080,
+            yval = 11;
+
+        var out = hoverPoints(pointData, xval, yval)[0];
+
+        expect(out.index).toEqual(0);
+        expect([out.x0, out.x1, out.y0, out.y1]).toBeCloseToArray([
+            -2627.555, -2625.555, 105.410, 107.410
+        ]);
+        expect(out.extraText).toEqual('(10°, 10°)<br>A');
+        expect(out.color).toEqual('#1f77b4');
+    });
+
+    it('should generate hover label info (hoverinfo: \'lon\' case)', function(done) {
+        Plotly.restyle(gd, 'hoverinfo', 'lon').then(function() {
+            var xval = 11,
+                yval = 11;
+
+            var out = hoverPoints(pointData, xval, yval)[0];
+
+            expect(out.extraText).toEqual('lon: 10°');
+            done();
+        });
+    });
+
+    it('should generate hover label info (hoverinfo: \'lat\' case)', function(done) {
+        Plotly.restyle(gd, 'hoverinfo', 'lat').then(function() {
+            var xval = 11,
+                yval = 11;
+
+            var out = hoverPoints(pointData, xval, yval)[0];
+
+            expect(out.extraText).toEqual('lat: 10°');
+            done();
+        });
+    });
+
+    it('should generate hover label info (hoverinfo: \'text\' case)', function(done) {
+        Plotly.restyle(gd, 'hoverinfo', 'text').then(function() {
+            var xval = 11,
+                yval = 11;
+
+            var out = hoverPoints(pointData, xval, yval)[0];
+
+            expect(out.extraText).toEqual('A');
+            done();
+        });
+    });
+});
